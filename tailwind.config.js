67a5b3693b5673aebb16ca17d8df461864827978
Finnerspx <finleypurnell98@gmail.com>
2019-11-24 15:26:36 +0000

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024',
+      'xl': '1280px',
+    }
+  
+  },
+  extend: {},
+  variants: {},
+  plugins: []
+}
